95cbe567a7850c623e2c59c490cd9211ef0e61c5
VladimirTulenev2325 <beatstours2325@yandex.ru>
2023-06-03 16:10:07 +0300

    1

---
+++ app/src/main/java/android/bignerdranch/vorota/MainActivity.java
@@ -0,0 +1,31 @@
+package android.bignerdranch.vorota;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bignerdranch.vorota.R;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Кнопка нажата!");
+            }
+        });
+    }
+}
